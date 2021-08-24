2314ef847903bc55fbf7d4a684f9052052a5559a
ogioldat <tomasz.ogiolda00@gmail.com>
2021-08-24 23:41:20 +0200

    update headers

--- src/controllers/salaries.js
+++ src/controllers/salaries.js
@@ -9,7 +9,10 @@ exports.getSalariesController = {
         try {
             const csv = await getSalaries({ date })
 
-            return h.response(csv).type('text/csv')
+            return h.response(csv)
+                .type('text/csv')
+                .header('Content-Type', 'text/csv')
+                .header('Content-Disposition', 'attachment;filename=salaries.csv')
         } catch (error) {
             return h.response({ error: `Invalid date param, please provide "date" param in the following format: YYYY-MM-DD` }).code(400)
         }
