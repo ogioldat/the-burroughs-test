6b2c7e87c391bf56a1df20e4910e9ef9708919d1
ogioldat <tomasz.ogiolda00@gmail.com>
2021-08-24 21:46:20 +0200

    tests for salary helpers

---
+++ src/helpers/salaries.spec.js
@@ -0,0 +1,36 @@
+const { getBaseSalaryDate, getBonusSalaryDate } = require('./salaries')
+const {format} = require('../utils/dates')
+
+describe('salaries helpers', () => {
+    describe('getBaseSalaryDate', () => {
+        test('it returns base salary date', async () => {
+            const date = new Date('2021-10-14')
+            const baseDate = getBaseSalaryDate(date)
+
+            expect(format(baseDate)).toEqual('2021-10-29')
+        })
+
+        test('it returns base salary date for weekends', async () => {
+            const date = new Date('2021-02-12')
+            const baseDate = getBaseSalaryDate(date)
+
+            expect(format(baseDate)).toEqual('2021-02-26')
+        })
+    })
+
+    describe('getBonusSalaryDate', () => {
+        test('it returns correct bonus date', async () => {
+            const date = new Date('2021-02-20')
+            const bonusDate = getBonusSalaryDate(date)
+
+            expect(format(bonusDate)).toEqual('2021-03-15')
+        })
+
+        test('it returns correct date for weekends', async () => {
+            const date = new Date('2021-12-12')
+            const bonusDate = getBonusSalaryDate(date)
+
+            expect(format(bonusDate)).toEqual('2022-01-19')
+        })
+    })
+})
