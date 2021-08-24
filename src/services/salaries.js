a11ed57aa000fa3283e04a71b45540dc68f8dc46
ogioldat <tomasz.ogiolda00@gmail.com>
2021-08-24 20:56:37 +0200

    get salaries csv from service

--- src/services/salaries.js
+++ src/services/salaries.js
@@ -1,37 +1,55 @@
-const { endOfMonth, addMonths } = require('date-fns')
-const { Parser } = require('json2csv')
-const { isWeekDay, getNearestDateByDOW, format } = require('../utils/dates')
+const { endOfMonth, addMonths } = require("date-fns");
+const { Parser } = require("json2csv");
+const {
+    isWeekDay,
+    getNearestDateByDOW,
+    format,
+    nthDayOfTheMonth,
+} = require("../utils/dates");
 
 const getBaseSalaryDate = (date) => {
-    const lastDayOfMonth = endOfMonth(date)
+    const lastDayOfMonth = endOfMonth(date);
 
     if (isWeekDay(lastDayOfMonth)) {
-        return getNearestDateByDOW(lastDayOfMonth, 'Friday')
+        return getNearestDateByDOW(lastDayOfMonth, "Friday");
     }
-    return lastDayOfMonth
-}
+    return lastDayOfMonth;
+};
+
+const getBonusSalaryDate = (date) => {
+    const nextMonthsDay = addMonths(date, 1)
+    const fifteenthDay = nthDayOfTheMonth(nextMonthsDay, 15 - 1);
+
+    if (isWeekDay(fifteenthDay)) {
+        return getNearestDateByDOW(fifteenthDay, "Wednesday", {
+            pastDates: false,
+        });
+    }
+    return fifteenthDay;
+};
 
 exports.getSalaries = (input) => {
-    let currentDate = input.date
-    const monthsInterval = 12
+    let currentDate = input.date;
+    const monthsInterval = 12;
 
-    const fields = ['baseSalary', 'bonusSalary']
-    const json2csv = new Parser({ fields })
+    const fields = ["baseSalary", "bonusSalary"];
+    const json2csv = new Parser({ fields });
 
-    const result = []
+    const result = [];
 
     for (let i = 0; i < monthsInterval; i++) {
-        const baseSalaryDate = getBaseSalaryDate(currentDate)
+        const baseSalaryDate = getBaseSalaryDate(currentDate);
+        const bonusSalaryDate = getBonusSalaryDate(currentDate);
 
         result.push({
             baseSalary: format(baseSalaryDate),
-            bonusSalary: null
-        })
+            bonusSalary: format(bonusSalaryDate),
+        });
 
-        currentDate = addMonths(currentDate, 1)
+        currentDate = addMonths(currentDate, 1);
     }
 
-    const csv = json2csv.parse(result)
+    const csv = json2csv.parse(result);
 
-    return csv
-}
+    return csv;
+};
--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,19 +1,29 @@
-const { format, subDays } = require("date-fns");
+const { format, subDays, addDays, startOfMonth } = require("date-fns");
 
 const WEEKEND_DAYS = ["Saturday", "Sunday"];
 
 exports.getDayName = (date = new Date()) => format(date, "EEEE");
 
-exports.isWeekDay = (date = new Date()) => WEEKEND_DAYS.includes(this.getDayName(date));
+exports.isWeekDay = (date = new Date()) =>
+    WEEKEND_DAYS.includes(this.getDayName(date));
 
-exports.getNearestDateByDOW = (date = new Date(), dow = "Monday") => {
-    while (
-        this.getDayName(date).toLowerCase() !== dow.toLowerCase()
-    ) {
-        date = subDays(date, 1)
+exports.getNearestDateByDOW = (
+    date = new Date(),
+    dow = "Monday",
+    { pastDates } = { pastDates: true }
+) => {
+    const traverseDay = pastDates ? subDays : addDays
+    
+    while (this.getDayName(date).toLowerCase() !== dow.toLowerCase()) {
+        date = traverseDay(date, 1);
     }
 
-    return date
+    return date;
 };
 
-exports.format = (date = new Date()) => format(date, 'yyyy-MM-dd')
+exports.format = (date = new Date()) => format(date, "yyyy-MM-dd");
+
+exports.nthDayOfTheMonth = (date = new Date(), n = 0) => {
+    const firstDay = startOfMonth(date)
+    return addDays(firstDay, n)
+}
