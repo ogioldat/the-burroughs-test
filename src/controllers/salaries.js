0374589900c51c90bff0b88c5b7642b3062ca514
ogioldat <tomasz.ogiolda00@gmail.com>
2021-08-24 23:39:53 +0200

    handle param error

--- src/controllers/salaries.js
+++ src/controllers/salaries.js
@@ -5,8 +5,13 @@ exports.getSalariesController = {
     description: "Get employee's salary",
     async handler(req, h) {
         const { date } = req.query
-        const csv = await getSalaries({ date })
 
-        return h.response(csv).type('text/csv')
+        try {
+            const csv = await getSalaries({ date })
+
+            return h.response(csv).type('text/csv')
+        } catch (error) {
+            return h.response({ error: `Invalid date param, please provide "date" param in the following format: YYYY-MM-DD` }).code(400)
+        }
     },
 }
